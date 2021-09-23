2d8de7dcbf9e8f399b9dbb5f0af59ce83a71b0e1
PhuongHieuHo <fuhi.dev@gmail.com>
2021-09-23 17:24:39 +0700

    test-app

--- test-app/src/app.module.ts
+++ test-app/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GeometryModule } from 'nestjs-gis';
 
 @Module({
-  imports: [],
+  imports: [
+    // CayXanhModule,
+    TypeOrmModule.forRoot({
+      options: { encrypt: false },
+      type: 'mssql',
+      host: '171.244.32.245',
+      port: 1433,
+      username: 'sa',
+      password: 'Ditagis123',
+      database: 'KhuCongNgheCao',
+      synchronize: false,
+      logging: false,
+      entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+      subscribers: [__dirname + '/../../**/*.subscriber{.ts,.js}'],
+    }),
+    GeometryModule.forRoot({
+      srs:{wkid:4326},arcService:'http://171.244.32.245/arcgis'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
